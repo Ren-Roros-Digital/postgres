1d8859132a5ea678f17b8a342152236abdae4d39
Etienne Stalmans <etienne@supabase.io>
2025-08-15 15:00:07 +0200

    feat: switch to include directives in pg_hba
    Using include directives makes changing the pg_hba.conf on the fly more
    flexible. Enabling / disabling ssl enforcement for example only requires
    creating or removing a file, leaving the pg_hba.conf untouched. Allowing
    for more repeatable and stable processes and no need for regex based
    replace or custom parsers.
    
    This will also support the just-in-time access work by allowing jit to
    be dynamically enabled/disabled

--- nix/packages/lib.nix
+++ nix/packages/lib.nix
@@ -52,6 +52,14 @@
           name = "pg_hba.conf";
           path = ../../ansible/files/postgresql_config/pg_hba.conf.j2;
         };
+        pgHbaUsersPublicConfigFile = builtins.path {
+          name = "pg_hba_users_public.conf";
+          path = ../../ansible/files/postgresql_config/pg_hba_users_public.conf.j2;
+        };
+        pgHbaPublicConfigFile = builtins.path {
+          name = "pg_hba_public.conf";
+          path = ../../ansible/files/postgresql_config/pg_hba_public.conf.j2;
+        };
         pgIdentConfigFile = builtins.path {
           name = "pg_ident.conf";
           path = ../../ansible/files/postgresql_config/pg_ident.conf.j2;
@@ -115,6 +123,8 @@
         cp ${paths.loggingConfigFile} $out/etc/postgresql-custom/logging.conf || { echo "Failed to copy logging.conf"; exit 1; }
         cp ${paths.readReplicaConfigFile} $out/etc/postgresql-custom/read-replica.conf || { echo "Failed to copy read-replica.conf"; exit 1; }
         cp ${paths.pgHbaConfigFile} $out/etc/postgresql/pg_hba.conf || { echo "Failed to copy pg_hba.conf"; exit 1; }
+        cp ${paths.pgHbaUsersPublicConfigFile} $out/etc/postgresql/pg_hba_users_public.conf || { echo "Failed to copy pg_hba_users_public.conf"; exit 1; }
+        cp ${paths.pgHbaPublicConfigFile} $out/etc/postgresql/pg_hba_public.conf || { echo "Failed to copy pg_hba_public.conf"; exit 1; }
         cp ${paths.pgIdentConfigFile} $out/etc/postgresql/pg_ident.conf || { echo "Failed to copy pg_ident.conf"; exit 1; }
         cp -r ${paths.postgresqlExtensionCustomScriptsPath}/* $out/extension-custom-scripts/ || { echo "Failed to copy custom scripts"; exit 1; }
 
@@ -123,6 +133,8 @@
         chmod 644 $out/etc/postgresql/postgresql.conf
         chmod 644 $out/etc/postgresql-custom/logging.conf
         chmod 644 $out/etc/postgresql/pg_hba.conf
+        chmod 644 $out/etc/postgresql/pg_hba_users_public.conf
+        chmod 644 $out/etc/postgresql/pg_hba_public.conf
 
         substitute ${../tools/run-server.sh.in} $out/bin/start-postgres-server \
           ${
