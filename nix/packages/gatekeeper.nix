e873fd149dd2e16bb9c0ff3af270e41b973efbec
Etienne Stalmans <etienne@supabase.io>
2025-08-26 17:25:56 +0200

    fix: fmt and go version, hopefully

--- nix/packages/gatekeeper.nix
+++ nix/packages/gatekeeper.nix
@@ -3,7 +3,15 @@
   perSystem =
     { system, ... }:
     let
-      go124 = inputs.nixpkgs-go124.legacyPackages.${system}.go_1_24;
+
+      go124 =
+        let
+          candidate = inputs.nixpkgs-go124.legacyPackages.${system};
+        in
+        if candidate ? go_1_24 then
+          candidate.go_1_24
+        else
+          throw "❌ nixpkgs-go124.${system} does not provide go_1_24!";
     in
     {
       packages.gatekeeper = inputs.gatekeeper.lib.${system}.makeGatekeeper { go = go124; };
