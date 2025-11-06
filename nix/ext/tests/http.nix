537ceef13c4a2243a12016afedc1109a582e49a1
Jean-François Roche <jfroche@pyxel.be>
2025-11-06 17:25:45 +0100

    chore(ext): document exceptions to generic extension tests
    This helps us identify why certain extensions do not use the generic tests.

--- nix/ext/tests/http.nix
+++ nix/ext/tests/http.nix
@@ -154,3 +154,6 @@ self.inputs.nixpkgs.lib.nixos.runTest {
       check_upgrade_path("17")
     '';
 }
+# We don't use the generic test for this extension because:
+# http is not using semver versioning scheme, so we need to adapt the version checks
+# otherwise the test fails with ERROR:  extension "http" has no installation script nor update path for version "1.5.0"
